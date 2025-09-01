2519e74dcbcdddcae8cc11b84753ed15cfb3a841
amthree <mthree9999@gmail.com>
2025-09-01 14:31:09 +0630

    sitemap fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'nextportfolio-two-amber.vercel.app',
+    siteUrl: process.env.SITE_URL || 'https://nextportfolio-two-amber.vercel.app/',
     generateRobotsTxt: true,
     sitemapSize: 7000,
 };
